28179ba5facbe3297685f282fde0f5a920c43a1e
Taisiia Hulko <hulko.taisiia@chnu.edu.ua>
2025-05-13 13:26:35 +0300

    ..

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -11,6 +11,7 @@ class Program
         Console.WriteLine("2 - Видалити українські слова, що починаються на голосну");
         Console.WriteLine("3 - Вилучити попередні входження останньої літери в кожному слові");
         Console.WriteLine("4 - Записати у двійковий файл числа з інтервалу");
+        Console.WriteLine("5 - Операції з директоріями та файлами (Гулько)");
         Console.Write("Ваш вибір: ");
         string choice = Console.ReadLine();
 
@@ -27,6 +28,9 @@ class Program
                 break;
             case "4":
                 Task4_BinaryWriteAndRead();
+                break;
+            case "5":
+                Task5_FileOperations();
                 break;
             default:
                 Console.WriteLine("Невірний вибір. Спробуйте знову.");
@@ -203,5 +207,47 @@ class Program
         }
     }
 
+    static void Task5_FileOperations()
+    {
+        string basePath = @"D:\temp";
+        string path1 = Path.Combine(basePath, "Гулько1");
+        string path2 = Path.Combine(basePath, "Гулько2");
 
+        Directory.CreateDirectory(path1);
+        Directory.CreateDirectory(path2);
+
+        string t1Path = Path.Combine(path1, "t1.txt");
+        string t2Path = Path.Combine(path1, "t2.txt");
+        File.WriteAllText(t1Path, "Шевченко Степан Іванович, 2001 року народження, місце проживання м. Суми");
+        File.WriteAllText(t2Path, "Комар Сергій Федорович, 2000 року народження, місце проживання м. Київ");
+
+        string t3Path = Path.Combine(path2, "t3.txt");
+        File.WriteAllText(t3Path, File.ReadAllText(t1Path) + Environment.NewLine + File.ReadAllText(t2Path));
+
+        Console.WriteLine("Інформація про створені файли:");
+        foreach (var file in new[] { t1Path, t2Path, t3Path })
+        {
+            FileInfo fi = new FileInfo(file);
+            Console.WriteLine($"{fi.FullName} | Розмір: {fi.Length} байт | Створено: {fi.CreationTime}");
+        }
+
+        string movedT2 = Path.Combine(path2, "t2.txt");
+        File.Move(t2Path, movedT2);
+
+        string copiedT1 = Path.Combine(path2, "t1.txt");
+        File.Copy(t1Path, copiedT1);
+
+        string allPath = Path.Combine(basePath, "ALL");
+        if (Directory.Exists(allPath)) Directory.Delete(allPath, true);
+        Directory.Move(path2, allPath);
+        Directory.Delete(path1, true);
+
+        Console.WriteLine("\nФайли в папці ALL:");
+        string[] finalFiles = Directory.GetFiles(allPath);
+        foreach (string file in finalFiles)
+        {
+            FileInfo fi = new FileInfo(file);
+            Console.WriteLine($"{fi.Name} | {fi.Length} байт | {fi.CreationTime}");
+        }
+    }
 }
